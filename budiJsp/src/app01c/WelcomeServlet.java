baa405fbe931a8630c492393905f7bf45375d2c9
Kirtana Yeluripati <kirtanaway@gmail.com>
2017-02-13 14:11:41 -0500

    WelcomeServlet using deployment discriptor

---
+++ budiJsp/src/app01c/WelcomeServlet.java
@@ -0,0 +1,23 @@
+package app01c;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "WelcomeServlet", urlPatterns = { "/welcome" })
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 27126L;
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.print("<html><head></head>"
+                + "<body>Welcome</body></html>");
+    }
+}
