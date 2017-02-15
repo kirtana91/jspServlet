3bca174f86fb95f4f18097c2bc026886db5b1720
Kirtana Yeluripati <kirtanaway@gmail.com>
2017-02-15 11:18:08 -0500

    Using JSTL

---
+++ budiJsp/src/app05/servlet/BooksServlet.java
@@ -0,0 +1,35 @@
+package app05.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import app05.model.Book;
+
+@WebServlet("/books")
+public class BooksServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		List<Book> books = new ArrayList<Book>();
+		Book book1 = new Book("978-0980839616", "Java 7: A Beginner's Tutorial", 45.00);
+		Book book2 = new Book("978-0980331608", "Struts 2 Design and Programming: A Tutorial", 49.95);
+		Book book3 = new Book("978-0975212820", "Dimensional Data Warehousing with MySQL: A " + "Tutorial", 39.95);
+		books.add(book1);
+		books.add(book2);
+		books.add(book3);
+		request.setAttribute("books", books);
+		RequestDispatcher rd = request.getRequestDispatcher("/app05/books.jsp");
+		rd.forward(request, response);
+	}
+
+}
